a865a7155741ee5a9e22afb86fbfab95ea65971a
Jialan Chen <jchen52@stuy.edu>
2024-11-26 14:50:24 -0500

    cw

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 
 int main() {
   pid_t p;
+  pid_t p2;
   p = fork();
   if (p<0) {
     perror("error with forking");
     exit(1);
   }
-  if (p==0) {
-    printf("%d %d sec", getpid())
-    //child
-
+  //child
+  if (p==0 || p2==0) {
+    srand(getpid());
+    int sec = rand() % (5) + 1;
+    printf("%d %d sec\n", getpid(), sec);
+    sleep(sec);
+    printf("%d finished after %d sec", getpid(), sec);
   }
+  //parent
   else {
-    printf("%d about to create 2 child processes", getpid());
-    //parent
+    printf("%d about to create 2 child processes\n", getpid());
+    p2 = fork();
+    int status;
+    int n = wait(&status);
+    if(WIFEXITED(status)) {
+      printf("Main Process %d is done. Child %d slept for a sec\n",
+        getpid(), n);
+    }
   }
+  return 0;
 }
---
+++ makefile
@@ -0,0 +1,9 @@
+compile: main.o
+	@gcc -o program  main.o
+run: program
+	@./program
+main.o: main.c
+	@gcc -c main.c
+clean:
+	@rm *.o
+	@rm program
