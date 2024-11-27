b0eada419c49b50f83a16a472b505ed410bf5453
Jialan <jialanc@nycstudents.net>
2024-11-26 23:13:21 -0500

    finished lab

--- main.c
+++ main.c
@@ -4,31 +4,32 @@
 #include <sys/wait.h>
 
 int main() {
-  pid_t p;
-  pid_t p2;
-  p = fork();
-  if (p<0) {
+  pid_t p, p2;
+  int sec;
+  printf("%d about to create 2 child processes\n", getpid());
+  (p = fork()) && (p2 = fork());
+  if (p<0 || p2<0) {
     perror("error with forking");
     exit(1);
   }
   //child
   if (p==0 || p2==0) {
     srand(getpid());
-    int sec = rand() % (5) + 1;
+    sec = rand() % 5 + 1;
     printf("%d %d sec\n", getpid(), sec);
     sleep(sec);
-    printf("%d finished after %d sec", getpid(), sec);
-  }
+    printf("%d finished after %d sec\n", getpid(), sec);
+    exit(sec);
+    }
   //parent
   else {
-    printf("%d about to create 2 child processes\n", getpid());
-    p2 = fork();
     int status;
-    int n = wait(&status);
+    pid_t n = wait(&status);
     if(WIFEXITED(status)) {
-      printf("Main Process %d is done. Child %d slept for a sec\n",
-        getpid(), n);
+      printf("Main Process %d is done. Child %d slept for %d sec\n",
+        getpid(), n, WEXITSTATUS(status));
     }
   }
   return 0;
 }
+
